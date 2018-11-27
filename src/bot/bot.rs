bfec59f6416714a5555a287782f3e5246b3ed122
markus <markus@openjob.co>
2018-11-27 22:33:44 +0100

    moved location of "filter_rules.json" to a dedicated folder under the systems config folder

--- src/bot/bot.rs
+++ src/bot/bot.rs
@@ -1,4 +1,7 @@
 
+use crate::storage::get_filters_from_storage;
+
 pub fn bot_invocation() {
     println!("bot invocation goes here");
+    let _filters = get_filters_from_storage();
 }
--- src/storage/storage.rs
+++ src/storage/storage.rs
@@ -4,16 +4,25 @@ use std::io::prelude::*;
 use std::path::Path;
 use crate::models::FilterRule;
 
+use log::*;
+
+extern crate dirs;
+
 pub fn read_file(filepath: &str) -> String {
-    let file = File::open(filepath)
-        .expect("could not open file");
+    let file = match File::open(filepath) {
+        Err(_why) => {
+            write_file(&"[]", filepath);
+            panic!("couldn't read filters at {:?}", filepath)
+        },
+        Ok(file) => file
+    };
     let mut buffered_reader = BufReader::new(file);
     let mut contents = String::new();
     let _number_of_bytes: usize = match buffered_reader.read_to_string(&mut contents) {
         Ok(number_of_bytes) => number_of_bytes,
         Err(_err) => 0
     };
-
+    info!("read file {} successfully", filepath);
     contents
 }
 
@@ -33,13 +42,21 @@ pub fn write_file(content: &str, filepath: &str) {
             panic!("couldn't write to {}: {}", display,
                    why)
         },
-        Ok(_) => println!("successfully wrote to {}", display),
+        Ok(_) => info!("successfully wrote to {}", display),
     }
 }
 
+/// assembles a file path to the filter rules
+/// location
+fn assemble_filter_target() -> String {
+    let mut target = dirs::config_dir().unwrap();
+    target.push("twitterbot-rs");
+    target.push("filter_rules.json");
+    target.into_os_string().into_string().unwrap()
+}
 
 pub fn get_filters_from_storage() -> Vec<FilterRule> {
-    let file_content: String = read_file("./filter_rules.json");
+    let file_content: String = read_file(assemble_filter_target().as_str());
     match serde_json::from_str(&file_content) {
         Ok(filters_converted) => filters_converted,
         Err(_err) => Vec::new()
@@ -48,5 +65,5 @@ pub fn get_filters_from_storage() -> Vec<FilterRule> {
 
 pub fn persist_filters_to_storage(filters: Vec<FilterRule>) {
     let content_str = serde_json::to_string_pretty(&filters).unwrap();
-    write_file(content_str.as_str(), "./filter_rules.json");
+    write_file(content_str.as_str(), &assemble_filter_target().as_str());
 }
